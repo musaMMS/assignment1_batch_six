f5f0b172ff77eb6affd6f3e546c961f9fcd913a6
Developer MuSa <mdmim018islam@gmail.com>
2024-03-09 21:51:16 +0600

    update

---
+++ bin/assignment1_batch_six.dart
@@ -0,0 +1,23 @@
+class Media {
+  void play() {
+    print("Playing media");
+  }
+}
+
+class Song extends Media {
+  String artist;
+  Song(this.artist);
+
+  @override
+  void play() {
+    print("Playing song by $artist");
+  }
+}
+
+void main() {
+  Media mediaInstance = Media();
+  Song songInstance = Song("ArtistName");
+
+  mediaInstance.play();
+  songInstance.play();
+}
